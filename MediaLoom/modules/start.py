9803096618298e1135b821b2b10a1f9b96c80903
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Akatsumo@users.noreply.github.com>
2026-03-02 12:58:55 +0530

    Update start.py

--- MediaLoom/modules/start.py
+++ MediaLoom/modules/start.py
@@ -5,20 +5,29 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInf
 
 
 buttons = InlineKeyboardMarkup([
-                [
-                  InlineKeyboardButton("🌐 Open WebApp", web_app=WebAppInfo(url=f"{config.WEB_URL}medialoom")
-                )],[
-                  InlineKeyboardButton("Repository", url="https://github.com/Akatsumo/MediaLoom")
-                ]])
+    [
+        InlineKeyboardButton(
+            "🌐 Open WebApp",
+            web_app=WebAppInfo(
+                url=f"{config.WEB_URL.rstrip('/')}/medialoom"
+            )
+        )
+    ],
+    [
+        InlineKeyboardButton(
+            "Repository",
+            url="https://github.com/Akatsumo/MediaLoom"
+        )
+    ]
+])
 
 
 @app.on_message(filters.command("start"))
 async def start(_, message):
     if message.chat.type == enums.ChatType.PRIVATE:
-# if you want add photo replace this url  await message.reply_photo(photo="https://graph.org/file/371c915b8c97ecf62a2d6-edbae9f1a9a3ff4add.jpg",
-        await message.reply_text(f"Hello, I'm {BOT_NAME}! I can host any type of media, including large files, with unlimited storage. Feel free to upload your files, and I'll take care of the rest!",
+        await message.reply_text(
+            f"Hello, I'm {BOT_NAME}! I can host any type of media, including large files, with unlimited storage. Feel free to upload your files, and I'll take care of the rest!",
             reply_markup=buttons
         )
     else:
         await message.reply_text("I am Alive!!")
-
